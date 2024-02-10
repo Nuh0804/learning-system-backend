14e99a3f95edd4d702c467133cd0da8e6828bd54
Nuh0804 <nuh.saidi.buhero@gmail.com>
2024-02-10 17:04:19 +0300

    bug fixes

--- learning_management_system/learning_management_system/settings.py
+++ learning_management_system/learning_management_system/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'learning_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'learning_system',
-        'HOST': 'localhost',
-        'USER': 'postgres',
-        'PASSWORD': 'nuhusaidi',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
